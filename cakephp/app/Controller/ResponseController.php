4dc3a9a9bcaab2f41f43d399acaf34d336fa7e86
Wataru Koshikawa <wataru.koshikawa@inter-plug.co.jp>
2015-06-12 18:54:00 +0900

    response GETでもらった値からスレ一覧をTBから取得まで

--- cakephp/app/Controller/ResponseController.php
+++ cakephp/app/Controller/ResponseController.php
@@ -5,6 +5,40 @@ APP::uses('AppController', 'Controller');
 class ResponseController extends AppController{
 	//レス一覧、表示
 	public function show_main(){
+		$this->loadModel('Response_tb');
+		$get_id = $this->params['url'];
+		$sql = "	
+					SELECT 
+						account_tbs.name ,
+						get_thread.response_id ,
+						get_thread.response_text ,
+						get_thread.response_date ,
+						get_thread.account_id ,
+						get_thread.thread_title ,
+						get_thread.thread_text
+					FROM 
+						`account_tbs` 
+					INNER JOIN 
+						(SELECT 
+							response_tbs.id AS response_id ,
+							response_tbs.text AS response_text ,
+							response_tbs.date AS response_date ,
+							response_tbs.account_id ,
+							thread_tbs.title AS thread_title ,
+							thread_tbs.text AS thread_text
+						FROM `response_tbs` 
+						INNER JOIN
+							thread_tbs
+						ON 
+							thread_tbs.id = response_tbs.thread_id
+						WHERE thread_id = ".$get_id['thread_id'].") AS get_thread 
+					ON 
+						get_thread.account_id = account_tbs.id
+				";
+
+		var_dump($sql);
+		$return = $this->Response_tb->query($sql);
+		$this->set("table_data", $return);
 		$this->render('response');
 	}
 	//投稿画面、表示
--- cakephp/app/View/response/response.ctp
+++ cakephp/app/View/response/response.ctp
@@ -1,6 +1,11 @@
+<pre>
+	
+	<?php  var_dump($table_data);?>
+</pre>
+
 <div id="title_area">
-	<p>スレタイ：〜〜〜〜</p>
-	<p>本文：〜〜〜〜〜</p>
+	<p>スレタイ：<?php echo $table_data[0]['get_thread']['thread_title'] ?></p>
+	<p>本文：<?php echo $table_data[0]['get_thread']['thread_text'] ?></p>
 </div>
 <!-- 区切り線をいれる -->
 <div id="response_area">
@@ -10,7 +15,12 @@
 	<?php 
 		//for文でレスをまわして表示
 		//以下HTMLは見本
+	$i = 0;
+	foreach ($table_data as $data) {
+		
+	}
 	?>
+
 	<div class="response_box">
 		<form action="run_dalete" method="POST">
 			<input type="submit" name="delete_res" value="削除">
@@ -25,7 +35,7 @@
 		<p>日付：〜〜〜〜〜</p>
 		<p>本文：〜〜〜〜〜</p>
 	</div>
-	
+
 	<form action="show_input" method="POST">
 		<input type="submit" name="input_res" value="レス投稿">
 	</form>
