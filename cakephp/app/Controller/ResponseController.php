b4c812bc45f47c2da0809927d29c6e7c556acfab
yudaisaito <yudai.saito@inter-plug.co.jp>
2015-06-15 13:29:01 +0900

    no message

--- cakephp/app/Controller/ResponseController.php
+++ cakephp/app/Controller/ResponseController.php
@@ -7,9 +7,11 @@ class ResponseController extends AppController{
 	public function show_main(){
 		$this->loadModel('Response_tb');
 		$get_id = $this->params['url'];
+<<<<<<< HEAD
+=======
 		var_dump($get_id);
-		$sql = "	
-					SELECT 
+		$sql = "
+					SELECT
 						account_tbs.name ,
 						get_thread.response_id ,
 						get_thread.response_text ,
@@ -17,46 +19,106 @@ class ResponseController extends AppController{
 						get_thread.account_id ,
 						get_thread.thread_title ,
 						get_thread.thread_text
-					FROM 
-						`account_tbs` 
-					INNER JOIN 
-						(SELECT 
+					FROM
+						`account_tbs`
+					INNER JOIN
+						(SELECT
 							response_tbs.id AS response_id ,
 							response_tbs.text AS response_text ,
 							response_tbs.date AS response_date ,
 							response_tbs.account_id ,
 							thread_tbs.title AS thread_title ,
 							thread_tbs.text AS thread_text
-						FROM `response_tbs` 
+						FROM `response_tbs`
 						INNER JOIN
 							thread_tbs
-						ON 
+						ON
 							thread_tbs.id = response_tbs.thread_id
-						WHERE thread_id = ".$get_id['thread_id'].") AS get_thread 
-					ON 
+						WHERE thread_id = ".$get_id['thread_id'].") AS get_thread
+					ON
 						get_thread.account_id = account_tbs.id
 					ORDER BY response_date ASC
 				";
+>>>>>>> response 初の画面表示データ取得から表示まで
 
-		var_dump($sql);
+		$check_account = array(
+							"name" => CakeSession::read('account_name'),
+							"id" => CakeSession::read('account_id')
+						);
+		$check_input = array(
+							"thread_id" => $get_id['thread_id'],
+							"account_id" => CakeSession::read('account_id')
+						);
+
+		$sql = "
+				SELECT
+					thread_tbs.id,
+					thread_tbs.title,
+					thread_tbs.text AS thread_text,
+					response_tbs.id AS response_id,
+					response_tbs.text AS response_text,
+					response_tbs.date,
+					account_tbs.name,
+					account_tbs.id AS account_id
+				FROM
+					thread_tbs
+				LEFT JOIN
+					response_tbs
+				ON
+					thread_tbs.id = response_tbs.thread_id
+				LEFT JOIN
+					account_tbs
+				ON
+					account_tbs.id = response_tbs.account_id
+				WHERE
+					thread_tbs.id = " . $get_id['thread_id'] . "
+				ORDER BY
+					response_tbs.date
+				ASC
+
+				";
 		$return = $this->Response_tb->query($sql);
 		$this->set("table_data", $return);
+		$this->set("account", $check_account);
+		$this->set("check_input", $check_input);
 		$this->render('response');
 	}
 	//投稿画面、表示
 	public function show_input(){
+		$this->set("check_input",$this->request->data);
 		$this->render('response_input');
 	}
 	//投稿画面、投稿
 	public function run_input(){
+		$this->loadModel('Response_tb');
+		$where = $this->request->data;
+		$arr = array(
+					'text' => $where['input_response'],
+					'date' => date('Y-m-d H:i:s'),
+					'account_id' => (int)$where['account_id'],
+					'thread_id' => (int)$where['thread_id']
+				);
+		var_dump($arr);
+		$this->Response_tb->set($arr);
+		$this->Response_tb->save();
+		$this->redirect('show_main?thread_id='.$where['thread_id'].'');
 
 	}
 	//一覧画面、スレに戻る
 	public function return_thread(){
-
+		$this->redirect('../thread/thread');
 	}
 	//一覧画面、削除
 	public function run_dalete(){
+		$this->loadModel('Response_tb');
+		$where = $this->request->data;
+		$this->Response_tb->delete($where['response_id']);
+		$this->redirect('show_main?thread_id='.$where['thread_id'].'');
 
+	}
+	//レス投稿画面、レス一覧に戻る
+	public function return_response(){
+		$this->loadModel('Response_tb');
+		$this->redirect("show_main?thread_id=". $this->request->data['thread_id']);
 	}
 };
--- cakephp/app/View/response/response.ctp
+++ cakephp/app/View/response/response.ctp
@@ -1,5 +1,120 @@
+<<<<<<< HEAD
+<style>
+	div#main_area{
+		width: 500px;
+		margin: 0 auto;
+	}
+	p.title_text{
+		font-size: 30px;
+		margin: 0;
+	}
+	div#button_area{
+		height: 50px;
+	}
+	div#response_regist{
+		float: left;
+	}
+	div#return_thread{
+		float: right;
+		margin-right: -20px;
+	}
+	div.response_box{
+		margin-top: 50px
+		border:solid;
+	}
+	div#response_text{
+		float: left;
+		width: 300px;
+	}
+	div#response_delete{
+		float: right;
+		width: 150px;
+		margin-bottom: 0;
+	}
+</style>
+<div id="main_area">
+	<div id="title_area">
+		<table style="width:500px">
+			<tr>
+				<th style="padding:0; width:120px; ">
+					<p class="title_text">スレタイ：</p>
+				</th>
+				<th>
+					<p class="title_text"><?php echo $table_data[0]['thread_tbs']['title'] ?></p>
+				</th>
+			</tr>
+			<tr>
+				<th style="text-align:right;">
+					<p class="title_text">本文：</p>
+				</th>
+				<th>
+					<p class="title_text"><?php echo $table_data[0]['thread_tbs']['thread_text'] ?></p>
+				</th>
+			</tr>
+		</table>
+	</div>
+	<!-- 区切り線をいれる -->
+	<div id="response_area">
+		<div id="button_area">
+			<div id="response_regist">
+				<form action="show_input" method="POST">
+					<input type="hidden" name="account_id" value="<?php echo $check_input['account_id'] ?>">
+					<input type="hidden" name="thread_id" value="<?php echo $check_input['thread_id'] ?>">
+					<input type="submit" name="input_res" value="レス投稿">
+				</form>
+			</div>
+			<div id="return_thread">
+				<form action="return_thread" method="POST">
+					<input type="submit" name="return_thread" value="スレ一覧に戻る">
+				</form>
+			</div>
+		</div>
+		<hr><br>
+		<?php
+			//for文でレスをまわして表示
+		foreach ($table_data as $data):
+			if($data['response_tbs']['response_text']):
+		?>
+				<div class='response_box'>
+					<div id="response_text">
+						<table>
+							<tr><th>日付：<?php echo $data['response_tbs']['date']; ?></th></tr>
+							<tr><th style="word-break:break-all;">本文：<?php echo $data['response_tbs']['response_text']; ?></th></tr>
+						</table>
+					</div>
+
+			<?php
+				if($account['id'] == $data['account_tbs']['account_id'] && $account['name'] == $data['account_tbs']['name']):
+			?>
+					<div id="response_delete">
+						<form action='run_dalete' method='POST'>
+							<input type="hidden" name="thread_id" value="<?php echo $check_input['thread_id'] ?>">
+							<input type="hidden" name="response_id" value="<?php echo $data['response_tbs']['response_id'] ?>">
+							<input type='submit' name='delete_res' value='削除'>
+						</form>
+					</div>
+			<?php
+				endif;
+			?>
+
+
+				</div>
+		<?php
+			else:
+			endif;
+		endforeach;
+		?>
+
+		<form action="show_input" method="POST">
+			<input type="hidden" name="account_id" value="<?php echo $check_input['account_id'] ?>">
+			<input type="hidden" name="thread_id" value="<?php echo $check_input['thread_id'] ?>">
+			<input type="submit" name="input_res" value="レス投稿">
+		</form>
+	</div>
+</div>
+=======
 <pre>
-	
+
 	<?php  var_dump($table_data);?>
 </pre>
 <style type="text/css">
@@ -16,10 +131,10 @@
 	<form action="show_input" method="POST">
 		<input type="submit" name="input_res" value="レス投稿">
 	</form>
-	<?php 
+	<?php
 		//for文でレスをまわして表示
 		//以下HTMLは見本
-	
+
 	foreach ($table_data as $data) {
 		$box = '
 
@@ -38,3 +153,4 @@
 		<input type="submit" name="input_res" value="レス投稿">
 	</form>
 </div>
+>>>>>>> response 初の画面表示データ取得から表示まで
